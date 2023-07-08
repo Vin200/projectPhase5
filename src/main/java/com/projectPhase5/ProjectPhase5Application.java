18c1fa1057ca118720c7818c1f67028ebc5dbbc7
Vinayak Badiger <vinayakvinayak627@gmail.com>
2023-07-08 23:52:53 +0530

    Initial Commit

---
+++ src/main/java/com/projectPhase5/ProjectPhase5Application.java
@@ -0,0 +1,13 @@
+package com.projectPhase5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPhase5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPhase5Application.class, args);
+	}
+
+}
